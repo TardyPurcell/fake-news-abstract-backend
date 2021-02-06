4eb3df100808b475bea2cbff60e883b97da26be0
Tardy Purcell <chiyunfan@sina.com>
2021-02-06 22:07:08 +0800

    ...

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_cors import CORS
 from textrank4zh import TextRank4Sentence
 
 
 app = Flask(__name__)
-CORS(app, resources=r'/api/*')
 
 
 @app.route('/api/<text>')
@@ -17,4 +15,4 @@ def work(text):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
